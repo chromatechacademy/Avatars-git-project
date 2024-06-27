062e7a7e66e1c415e8c73c042da8907a3c24ddd0
iamdez99 <diegojuarezbusiness@gmail.com>
2024-06-26 21:26:40 -0400

    My third push

--- src/test/java/org/example/Testing.java
+++ src/test/java/org/example/Testing.java
@@ -3,6 +3,8 @@ package org.example;
 public class Testing {
 
     public static void main(String[] args) {
-        System.out.println("My third git class");
+        System.out.println("hello world");
+        System.out.println("I AM HAVING THE TIME OF MY LIFE");
+        System.out.println("I CANNOT WAIT TO GET A JOB");
     }
 }
